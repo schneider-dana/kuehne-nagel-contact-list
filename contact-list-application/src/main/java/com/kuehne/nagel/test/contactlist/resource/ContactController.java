c88255544053508d363a059bec5f4a4809604cba
schneider-dana <schneider.dana@outlook.com>
2022-09-10 19:09:16 +0300

    adds pagination

--- contact-list-application/src/main/java/com/kuehne/nagel/test/contactlist/resource/ContactController.java
+++ contact-list-application/src/main/java/com/kuehne/nagel/test/contactlist/resource/ContactController.java
@@ -1,14 +1,22 @@
 package com.kuehne.nagel.test.contactlist.resource;
 
+import com.kuehne.nagel.test.contactlist.domain.Contact;
 import com.kuehne.nagel.test.contactlist.service.ContactService;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
+
+import java.util.List;
+import java.util.Optional;
+import java.util.stream.IntStream;
+
+import static java.util.stream.Collectors.toList;
 
 @Controller
 @RequestMapping("/api/contacts")
@@ -17,9 +25,22 @@ public class ContactController {
     @Autowired
     private ContactService contactService;
 
-    @GetMapping
-    public String findAll(final Model model) {
-        model.addAttribute("contacts", contactService.findAll());
+    @RequestMapping(value = "", method = RequestMethod.GET)
+    public String findAll(@RequestParam("page") final Optional<Integer> page,
+                          @RequestParam("size") final Optional<Integer> size,
+                          final Model model) {
+        final int currentPage = page.orElse(1);
+        final int pageSize = size.orElse(5);
+
+        final Page<Contact> contactPage = contactService.findAll(PageRequest.of(currentPage - 1, pageSize));
+
+        model.addAttribute("contactPage", contactPage);
+
+        final int totalPages = contactPage.getTotalPages();
+        if (totalPages > 0) {
+            final List<Integer> pageNumbers = IntStream.rangeClosed(1, totalPages).boxed().collect(toList());
+            model.addAttribute("pageNumbers", pageNumbers);
+        }
 
         return "contacts";
     }
--- contact-list-server/src/main/java/com/kuehne/nagel/test/contactlist/service/ContactService.java
+++ contact-list-server/src/main/java/com/kuehne/nagel/test/contactlist/service/ContactService.java
@@ -5,10 +5,16 @@ import com.kuehne.nagel.test.contactlist.model.Person;
 import com.kuehne.nagel.test.contactlist.repository.PersonRepository;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageImpl;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 
+import java.awt.print.Book;
 import java.util.List;
 
+import static java.util.Collections.emptyList;
 import static java.util.stream.Collectors.toList;
 
 @Service
@@ -17,8 +23,23 @@ public class ContactService {
     @Autowired
     private PersonRepository personRepository;
 
-    public List<Contact> findAll() {
-        return personRepository.findAll().stream().map(ContactService::toContact).collect(toList());
+    public Page<Contact> findAll(final Pageable pageable) {
+        final int pageSize = pageable.getPageSize();
+        final int currentPage = pageable.getPageNumber();
+        final int startItem = currentPage * pageSize;
+
+        final List<Contact> allContacts =
+            personRepository.findAll().stream().map(ContactService::toContact).collect(toList());
+        final List<Contact> page;
+
+        if (allContacts.size() < startItem) {
+            page = emptyList();
+        } else {
+            final int toIndex = Math.min(startItem + pageSize, allContacts.size());
+            page = allContacts.subList(startItem, toIndex);
+        }
+
+        return new PageImpl<>(page, PageRequest.of(currentPage, pageSize), allContacts.size());
     }
 
     public List<Contact> findByName(final String name) {
